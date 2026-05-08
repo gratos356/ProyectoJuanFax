834c81ae21291e14108fbae012fb4070511bb9be
gratos356 <andressergio1210@gmail.com>
2026-05-08 09:41:17 -0500

    feat: finalisacion de la estructura y diseño de desktop y comienzo con la de movil de la vista de vendedores y correccion en las rutas de importaciones

--- JuanFaxProject/web/scrips/vendedor.js
+++ JuanFaxProject/web/scrips/vendedor.js
@@ -1,16 +1,9 @@
-const BttnesNav = document.querySelectorAll('.BttNavegacion');
-const BttSideBaar = document.querySelectorAll('.BttnSideBaar');
+const botones = document.querySelectorAll('.botones');
 
-BttnesNav.forEach((bttn) => {
-    bttn.addEventListener('click', () => {
-        BttnesNav.forEach((btn) => btn.classList.remove('BttNavegacionFocus'));
-        bttn.classList.add('BttNavegacionFocus');
-    });
-});
 
-BttSideBaar.forEach((bttn) => {
+botones.forEach((bttn) => {
     bttn.addEventListener('click', () => {
-        BttSideBaar.forEach((btn) => btn.classList.remove('BttnSideBaarFocus'));
-        bttn.classList.add('BttnSideBaarFocus');
+        botones.forEach((btn) => btn.classList.remove('botonesFocus'));
+        bttn.classList.add('botonesFocus');
     });
 });
